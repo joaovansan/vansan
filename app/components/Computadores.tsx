1aca2dd3925731444cbddea89cbcb141f2e0f5f1
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-12-06 00:32:28 +0000

    fix fix

--- app/components/Computadores.tsx
+++ app/components/Computadores.tsx
@@ -9,13 +9,8 @@ export default async function Computadores() {
     return (
         <main className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
             <div className="md:col-span-2 lg:col-span-3 mt-4 text-white text-center">
-	@@ -14,9 +14,9 @@ export default function Computadores() {
+	
                 </h2>
             </div>
-            {
-                rows.map((computador) => {
-                    return (
-                        <div key={computador.id} className="bg-[#4d4d4d] rounded-md pb-2">
-                            <a href="/curso_html.html">
-                                <img className="hover:scale-105" src={computador.url} alt="" />
-                                <div className="text-white text-center"></div>
+            
+    )        
