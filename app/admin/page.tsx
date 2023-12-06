d66fe3da2523a8f41c03feed8e96d486de6d4f23
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-12-06 01:23:45 +0000

    fix 123 123

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@ export default function Admin(){
     return (
         <div>
         <h1 className="text-center text-xl">Seja Bem Vindo a aba de administração</h1>
-        <a href="/admin/computadores">Listar computadores </a>
+        <a href="/admin/computador">Listar computadores </a>
         
       
     
