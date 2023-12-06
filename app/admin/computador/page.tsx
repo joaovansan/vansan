a1914c7bd43860ff44bdaceded5bb7c865fc2143
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-12-06 01:40:35 +0000

    Fixxxxx

--- app/admin/computador/page.tsx
+++ app/admin/computador/page.tsx
@@ -27,7 +27,7 @@ export default async function ListComputador() {
                                 <td>
                                     <form >
                                      <input type="text" hidden name="id" value={computador.id}/>   
-                                    <button formAction={deleteComputador}>Excluir</button>
+                                    <button variant = "destructive" formAction={deleteComputador}> Excluir</button>
                                     </form>
                                 
                                 </td> 
